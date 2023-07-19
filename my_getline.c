0eb71646fc104d86a8a260cb68a8167bd82f24fc
Aden1ke <sarahogunlalu@gmail.com>
2023-07-19 14:15:46 -0700

    getline using read a system function

--- my_getline.c
+++ my_getline.c
@@ -1,49 +1,81 @@
 #include "my_shell.h"
 /**
  * my_getline - my personal getline.
- * @argc: number of arguments.
- * @argv: arguments inputed.
+ * @buffer: string inputed.
+ * @n: length of string.
+ * @stream: file descriptor.
  * Return: 0n success 1.
  */
-ssize_t my_getline(char **buffer, size_t *n, FILE *stream)
+ssize_t my_getline(char **buffer, size_t *n, FILE *stream) {
+	initialize_buffer(buffer, n);
+	return (read_from_buffer(buffer, n, stream));
+}
+/**
+ * initialize_buffer - my personal getline.
+ * @buffer: string inputed.
+ * @n: length of string.
+ * Return: 0n success 1.
+ */
+void initialize_buffer(char **buffer, size_t *n)
 {
-	size_t i = 0;
-	char *result;
-	if (buffer == NULL || n == NULL || stream == NULL)
-	{
-		return (-1);
-	}
+	if (buffer == NULL || n == NULL)
+		return;
+
 	if (*buffer == NULL || *n == 0)
 	{
-		*n = 128; 
+		*n = BUFFER_SIZE;
 		*buffer = malloc(*n * sizeof(char));
 		if (*buffer == NULL)
 		{
-			return (-1);
+			exit(EXIT_FAILURE);
 		}
 	}
-	while ((result = fgets(*buffer + i, *n - i, stream)) != NULL)
+}
+/**
+ * read_from_buffer - my personal getline.
+ * @buffer: string inputed.
+ * @n: length of string.
+ * @stream: file descriptor.
+ * Return: 0n success 1.
+ */
+ssize_t read_from_buffer(char **buffer, size_t *n, FILE *stream)
+{
+	static char read_buffer[BUFFER_SIZE];
+	static size_t buffer_position = 0;
+	static ssize_t bytes_in_buffer = 0;
+	size_t i = 0;
+	char result;
+	int newline_found = 0;
+
+	while (!newline_found)
 	{
-		size_t len = strlen(*buffer + i);
-		i += len;
-		if ((*buffer)[i - 1] == '\n')
+		if ((ssize_t)buffer_position >= bytes_in_buffer)
 		{
-			(*buffer)[i - 1] = '\0';
-			break;
+			bytes_in_buffer = read(fileno(stream), read_buffer, BUFFER_SIZE);
+			buffer_position = 0;
+			if (bytes_in_buffer <= 0)
+				break;
 		}
-		if (i >= *n - 1)
+		result = read_buffer[buffer_position++];
+		(*buffer)[i++] = result;
+		if (result == '\n')
+			newline_found = 1;
+		if (i >= *n - 1) 
 		{
 			*n *= 2;
 			*buffer = realloc(*buffer, *n * sizeof(char));
 			if (*buffer == NULL)
 			{
-				return (-1);
+				exit(EXIT_FAILURE);
 			}
 		}
 	}
-		if (i == 0 && result == NULL)
-		{
-			return (-1);
-		}
-		return ((ssize_t)i);
+
+	if (i == 0 && bytes_in_buffer <= 0)
+	{
+		return (-1);
+	}
+
+	(*buffer)[i] = '\0';
+	return ((ssize_t)i);
 }
--- my_shell.h
+++ my_shell.h
@@ -8,7 +8,10 @@
 #include <sys/stat.h>
 #include <stdbool.h>
 #include <sys/wait.h>
+#define BUFFER_SIZE 1024
 void handle_arguments(char *line);
 int _strcmp(char *s1, char *s2);
 ssize_t my_getline(char **buffer, size_t *n, FILE *stream);
+ssize_t read_from_buffer(char **buffer, size_t *n, FILE *stream);
+void initialize_buffer(char **buffer, size_t *n);
 #endif
