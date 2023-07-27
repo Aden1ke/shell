dd0ac790038279c5f1c1e6be63c8c6b6932a8122
Benose13 <okoegualebenjamin38@gmail.com>
2023-07-26 19:23:48 -0700

    done

--- strings2.c
+++ strings2.c
@@ -15,3 +15,21 @@ char *_strcpy(char *dest, const char *str)
 	dest[i] = '\0';
 	return (dest);
 }
+
+/**
+ * free_arr - frees memory allocated to an array
+ * @arr: the array to be freed
+ * Return: void
+ */
+void free_arr(char **arr)
+{
+	int i;
+
+	if (!arr)
+		return;
+
+	for (i = 0; arr[i] != NULL; i++)
+		free(arr[i]);
+
+	free(arr);
+}
