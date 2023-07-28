5725ccf823ca5ec243ad788afd6ca8896223cb48
Aden1ke <sarahogunlalu@gmail.com>
2023-07-28 03:22:08 -0700

    cd comman

--- handle_args.c
+++ handle_args.c
@@ -19,20 +19,11 @@ int handle_arguments(char *line)
 	command[i] = NULL;
 	if (_strcmp(command[0], "exit") == 0)
 	{
-		if (i == 1)
-		{
-			return (0); 
-		}
-		else if (i == 2)
-		{
-			int status = _atoi(command[1]);
-			return (status);
-		}
-		else
-		{
-			perror("Usage: exit [status]");
-			return (-1);
-		}
+		return (handle_exit_command(command));
+	}
+	else if (_strcmp(command[0], "cd") == 0) 
+	{
+		return (handle_cd_command(command));
 	}
 	return (execve_helper(command[0], command));
 }
