dc2ce8adc3edf7d1125f522c600886469ab01a86
Aden1ke <sarahogunlalu@gmail.com>
2023-07-29 07:01:32 -0700

    meege

--- extra_functions.c
+++ extra_functions.c
@@ -31,6 +31,20 @@ int _atoi(const char *s)
 	return (num * sign);
 }
 /**
+ * _islower - Write a function that checks for lowercase character.
+ * @c:value to be checked.
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+/**
  * free_array - handle arguments
  * @array: string to break down
  * Return: void.
@@ -51,3 +65,4 @@ void free_array(char **array)
 
 	free(array);
 }
+
